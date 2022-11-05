9ac23d88f66b18a05f54224b09b86cb6067a587b
Solly <solobarine@gmail.com>
2022-11-05 23:14:09 +0100

    Make recomendations

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -5,6 +5,7 @@ const Categories = () => { //eslint-disable-line
   return (
     <div className="category">
       <h1 className="categoriesHeader">Categories</h1>
+      <button type="button" className="status">Check Status</button>
     </div>
   );
 };
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -5,7 +5,7 @@ const Nav = () => { //eslint-disable-line
   return (
     <div className="navBar">
       <ul className="navLinks">
-        <h3 className="navItem"><Link to="/">SollyBooks</Link></h3>
+        <h3 className="navItem">SollyBooks</h3>
         <h3 className="navItem"><Link to="/books">Books</Link></h3>
         <h3 className="navItem"><Link to="/categories">Categories</Link></h3>
       </ul>
