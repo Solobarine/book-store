64fbfd5d038b68d01358e1cd48db342020673659
Solly <solobarine@gmail.com>
2022-11-08 08:38:42 +0100

    Fix Linters

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -3,25 +3,29 @@ import { useReducer, useState } from 'react';
 const ACTIONS = {
   ADD_BOOK: 'add-book',
   REMOVE_BOOK: 'remove-book',
-}
+};
 
-const reducer = (books, actions) => {
+const reducer = (books, actions) => { //eslint-disable-line
   switch (actions.type) {
     case ACTIONS.ADD_BOOK:
-      return [...books, {id: Date.now(), Title: actions.payload.bookTitle, Author: actions.payload.bookAuthor}];
-      break;
+		  return [...books, { id: Date.now(),
+			  Title: actions.payload.bookTitle,
+			  Author: actions.payload.bookAuthor }]
     case ACTIONS.REMOVE_BOOK:
-      books.filter((book) => book.id != actions.payload.id);
+      books.filter((book) => book.id !== actions.payload.id);
       break;
     default:
       return books;
-      break;
   }
 };
 
-const [books, dispatch] = useReducer(reducer, []);
-const [bookTitle, setBookTitle] = useState('');
-const [bookAuthor, setBookAuthor] = useState('');
+const [books, dispatch] = useReducer(reducer, []); //eslint-disable-line
 
-export { ACTIONS, books, dispatch, bookTitle, bookAuthor, setBookTitle, setBookAuthor };
+const [bookTitle, setBookTitle] = useState(''); //eslint-disable-line
+
+const [bookAuthor, setBookAuthor] = useState(''); //eslint-disable-line
+
+
+export { ACTIONS, books, dispatch, bookTitle, bookAuthor, setBookTitle, setBookAuthor
+};
 export default reducer;
