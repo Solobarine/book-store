dbbe89b711c42f1d73bb17b9a0580bd90cb9331a
Solly <solobarine@gmail.com>
2022-11-09 12:25:51 +0100

    Fix linters

--- src/components/Books.js
+++ src/components/Books.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import {useSelector} from 'react-redux';
+import { useSelector } from 'react-redux';
 import Book from './Book';
 
 const Books = () => { //eslint-disable-line
   const store = useSelector((state) => state.books);
-  console.log(store)
-  const showBooks = store.map(book => (
+  const showBooks = store.map((book) => (
     <Book key={book.id} book={book} />
-  ))
+  ));
+
   return (
     <div className="bookList">
       { showBooks }
